c34a675201b6f38ccc6bcd6f23624c3a61c38215
santhosh.vel <santhosh.vel@zohocorp.com>
2021-04-27 20:57:41 +0530

    add framework

---
+++ PodTest2/PodTest2/PodTest2.h
@@ -0,0 +1,18 @@
+//
+//  PodTest2.h
+//  PodTest2
+//
+//  Created by Santhosh Kumar on 27/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodTest2.
+FOUNDATION_EXPORT double PodTest2VersionNumber;
+
+//! Project version string for PodTest2.
+FOUNDATION_EXPORT const unsigned char PodTest2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodTest2/PublicHeader.h>
+
+
---
+++ PodTest2/PodTest2/Validator.swift
@@ -0,0 +1,17 @@
+//
+//  Validator.swift
+//  PodTest2
+//
+//  Created by Santhosh Kumar on 27/04/21.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func sayHello(){
+        print("Hello guys")
+    }
+    public static func badWords(){
+        print("fuck You")
+    }
+}
